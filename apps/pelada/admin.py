e198b74dbdb9073d96105fe9abcd5801ee541b0b
Eric Costa <ericosta.dev@gmail.com>
2023-03-05 18:41:45 +0100

    feat: configurando o admin

--- apps/pelada/admin.py
+++ apps/pelada/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from apps.pelada.models import Pelada,PresencaGoleiro,PresencaJogador
+
 # Register your models here.
+class GoleiroInline(admin.TabularInline):
+    model = PresencaGoleiro
+
+class JogadorInline(admin.TabularInline):
+    model = PresencaJogador
+
+class PeladaAdmin(admin.ModelAdmin):
+    inlines = [
+        GoleiroInline,
+        JogadorInline,
+    ]
+
+
+admin.site.register(Pelada, PeladaAdmin)
