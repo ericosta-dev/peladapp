fd8098bea36b535bf39377a9ca14f03e2b18b9ae
Eric Costa <ericosta.dev@gmail.com>
2023-03-05 18:41:40 +0100

    feat: configurando o admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from apps.core.models import Configuracoes
+from apps.core.models import Configuracoes,Goleiro,Jogador
 
 # Register your models here.
 class ConfiguracoesAdmin(admin.ModelAdmin):
     pass
+
+class GoleiroAdmin(admin.ModelAdmin):
+    pass
+class JogadorAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Configuracoes, ConfiguracoesAdmin)
+admin.site.register(Goleiro, GoleiroAdmin)
+admin.site.register(Jogador, JogadorAdmin)
