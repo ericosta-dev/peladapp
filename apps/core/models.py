1f6bbda70c51ed7ce5feecc5202c9a4ee306aad7
Eric Costa <ericosta.dev@gmail.com>
2023-03-05 18:42:33 +0100

    feat: adicionando novos models de Pelada, PresencaJogador, PresencaGoleiro

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class Configuracoes(models.Model):
-        """
+    """
     Model que armazena as configurações da pelada, incluindo o nome da pelada, o horário em que ela ocorre, a mensalidade
     para jogadores mensalistas e o saldo em caixa. Possui os seguintes atributos:
 
@@ -15,15 +16,26 @@ class Configuracoes(models.Model):
         __str__(): Retorna o nome da pelada como uma string.
 
     """
-        
-    nome = models.TextField(max_length=50,verbose_name='Nome da pelada')
-    horario = models.TimeField(verbose_name='Horário da pelada')
-    mensalidade = models.DecimalField(max_digits=8, decimal_places=2,
-                                    verbose_name='Valor por mensalista')
-    saldo = models.DecimalField(max_digits=8, decimal_places=2,verbose_name='Saldo em caixa')
+
+    nome = models.TextField(max_length=50, verbose_name="Nome da pelada")
+    horario = models.TimeField(verbose_name="Horário da pelada")
+    vl_mensalidade = models.DecimalField(
+        max_digits=8, decimal_places=2, verbose_name="Valor por mensalista"
+    )
+    vl_avulso = models.DecimalField(
+        max_digits=8, decimal_places=2, verbose_name="Valor por avulso"
+    )
+    saldo = models.DecimalField(
+        max_digits=8, decimal_places=2, verbose_name="Saldo em caixa"
+    )
 
     def __str__(self):
         return self.nome
+
+    class Meta:
+        verbose_name = "Configuração"
+        verbose_name_plural = "Configurações"
+
 
 class Pessoa(models.Model):
     """
@@ -36,16 +48,22 @@ class Pessoa(models.Model):
         pix (str, opcional): Chave Pix da pessoa.
 
     """
-    nome = models.CharField(max_length=100,verbose_name='Nome')
-    telefone = models.CharField(max_length=15,null=True,verbose_name='Telefone')
-    pix = models.CharField(max_length=100,null=True,verbose_name='Chave Pix')
+
+    nome = models.CharField(max_length=100, verbose_name="Nome")
+    telefone = models.CharField(
+        max_length=15, null=True, verbose_name="Telefone", blank=True
+    )
+    pix = models.CharField(
+        max_length=100, null=True, verbose_name="Chave Pix", blank=True
+    )
 
     class Meta:
         abstract = True
 
+
 class Jogador(Pessoa):
     """
-    Representa um jogador, que é um integrante da pelada que joga na linha. 
+    Representa um jogador, que é um integrante da pelada que joga na linha.
     Herda da classe Pessoa e adiciona um campo para o tipo de jogador (avulso ou mensalista).
     Possui os seguintes atributos:
 
@@ -62,18 +80,25 @@ class Jogador(Pessoa):
     AVULSO = 1
     MENSALISTA = 2
     TIPOS_PESSOA_CHOICES = (
-        (AVULSO, 'Avulso'),
-        (MENSALISTA, 'Mensalista'),
+        (AVULSO, "Avulso"),
+        (MENSALISTA, "Mensalista"),
     )
 
-    tipo = models.IntegerField(default=1,choices=TIPOS_PESSOA_CHOICES,verbose_name='Tipo')
+    tipo = models.IntegerField(
+        default=1, choices=TIPOS_PESSOA_CHOICES, verbose_name="Tipo"
+    )
 
     def __str__(self):
-            return self.nome
+        return self.nome
+
+    class Meta:
+        verbose_name = "Jogador"
+        verbose_name_plural = "Jogadores"
+
 
 class Goleiro(Pessoa):
     """
-    Representa um goleiro, que é um tipo de jogador que tem a função de defender o gol. Herda da classe Pessoa e adiciona um
+    Representa um goleiro. Herda da classe Pessoa e adiciona um
     campo para o custo do goleiro. Possui os seguintes atributos:
 
     Atributos:
@@ -85,7 +110,8 @@ class Goleiro(Pessoa):
     Métodos:
         __str__(): Retorna o nome do goleiro como uma string.
     """
+
     custo = models.DecimalField(max_digits=8, decimal_places=2)
-    
+
     def __str__(self):
-            return self.nome
+        return self.nome
--- apps/pelada/models.py
+++ apps/pelada/models.py
@@ -1,6 +1,20 @@
 from django.db import models
-from core.models import Pessoa
+
+from apps.core.models import Goleiro, Jogador
 
 # Create your models here.
 class Pelada(models.Model):
-    data = models.DateField(verbose_name='Data de realização')
+    data = models.DateField(verbose_name="Data de realização")
+    saldo = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+
+
+class PresencaJogador(models.Model):
+    jogador = models.ForeignKey(Jogador, on_delete=models.CASCADE)
+    pelada = models.ForeignKey(Pelada, on_delete=models.CASCADE)
+    vl_pago = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+
+
+class PresencaGoleiro(models.Model):
+    goleiro = models.ForeignKey(Goleiro, on_delete=models.CASCADE)
+    pelada = models.ForeignKey(Pelada, on_delete=models.CASCADE)
+    vl_pago = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
